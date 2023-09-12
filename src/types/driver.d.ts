16c5aca75fe42fcdadd56004da5edcc67eb04ac1
felipe gomez <ggfelipegc@gmail.com>
2023-09-12 15:27:38 -0500

    refactor: add Flag type to country

--- src/types/driver.d.ts
+++ src/types/driver.d.ts
@@ -42,5 +42,10 @@ export type Country = {
   id: number;
   name: string;
   iso: string;
-  flag: string;
+  flag: Flag;
+};
+
+export type Flag = {
+  fullscreen: string;
+  widescreen: string;
 };
