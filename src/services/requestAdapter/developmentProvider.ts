fba8f5f5efa38d20cb0c0dca6c01039630035685
felipe gomez <ggfelipegc@gmail.com>
2023-09-13 14:07:19 -0500

    feat: implement team request

--- src/services/requestAdapter/developmentProvider.ts
+++ src/services/requestAdapter/developmentProvider.ts
@@ -1,13 +1,15 @@
 import { Response } from "../../types";
 import { RequestProvider } from "./requestProvider";
-import { Error, Driver } from "./mocks";
-import { DRIVER } from "../../constants";
+import { Error, Driver, Team } from "./mocks";
+import { DRIVER, TEAM } from "../../constants";
 
 export class DevelopmentProvider implements RequestProvider {
   async getRequest(url: string, _headers?: string): Promise<Response> {
     switch (url) {
       case DRIVER:
         return Driver;
+      case TEAM:
+        return Team;
       default:
         return Error;
     }
