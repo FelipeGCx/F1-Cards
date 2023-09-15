846acc0bd42d57f283d45fd302a3d1c8cb8b48b9
felipe gomez <ggfelipegc@gmail.com>
2023-09-15 18:38:36 -0500

    feat: implement props logic to cards

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,43 @@
 <script setup lang="ts">
-import CardDriver from './components/CardDriver.vue'
-import CardTeam from './components/CardTeam.vue'
+import { onMounted, ref } from "vue";
+import { TEAM, SUCCESS, DRIVER, TEAMS } from "./constants";
+import { RequestService } from "./services/requestAdapter/requestService";
+import { DevelopmentProvider } from "./services/requestAdapter/developmentProvider";
+import { Driver, Team } from "./types";
+import CardDriver from "./components/CardDriver.vue";
+import CardTeam from "./components/CardTeam.vue";
+
+const team = ref<Team | null>(null);
+const driver = ref<Driver | null>(null);
+const teams = ref<Team[] | null>(null);
+
+onMounted(async () => {
+  team.value = await getData<Team>(TEAM);
+  teams.value = await getData<Team[]>(TEAMS);
+  driver.value = await getData<Driver>(DRIVER);
+});
+const getData = async <T>(url: string): Promise<T | null> => {
+  const requestProvider = new DevelopmentProvider();
+  const requestService = new RequestService(requestProvider);
+  const requestResponse = await requestService.getRequest(url);
+  switch (requestResponse.status) {
+    case SUCCESS:
+      return requestResponse.data.results as T;
+    default:
+      return null;
+  }
+};
 </script>
 
 <template>
   <div>
-    <CardDriver />
-    <CardTeam />
+    <CardDriver :driver="driver" />
+    <CardTeam :team="team" />
+    <ul>
+      <li v-for="t in teams" :key="t.id">
+        <CardTeam :team="t" />
+      </li>
+    </ul>
   </div>
 </template>
 
