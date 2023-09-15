26a106163532b7fa19d96a8de0c06a9475fb3253
felipe gomez <ggfelipegc@gmail.com>
2023-09-15 18:37:07 -0500

    feat: add teams mocks

--- src/services/requestAdapter/mocks/index.ts
+++ src/services/requestAdapter/mocks/index.ts
@@ -1,5 +1,6 @@
 import Error from "./Error.json";
 import Driver from "./Driver.json";
 import Team from "./Team.json";
+import Teams from "./Teams.json";
 
-export { Error, Driver, Team};
+export { Error, Driver, Team, Teams};
